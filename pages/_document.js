3e0590521e0c341f021bdc11f1edde6410d4016d
Orsbert Ayesigye <45017033+Orsbert@users.noreply.github.com>
2021-10-06 22:09:46 +0300

    og image

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,14 @@ export default class extends Document {
 					<link rel="icon" href="/dp_tiny.png" />
           <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
           <meta property="og:title" content="Orsbert Ayesigye's Portfolio" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="http://orsbert.com" />
-          <meta property="og:image:url" content="/dp.png" />
           <meta property="og:description" content="I help brands & businesses design, develop, test, deploy and monitor web-based applications." />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://orsbert.com" />
+          <meta property="og:image:url" content="https://orsbert.com/dp.png" />
+          <meta property="og:image" content="https://orsbert.com/dp.png" />
+          <meta property="og:image:secure_url" content="https://orsbert.com/dp.png" />
+          <meta property="og:image:width" content="343" />
+          <meta property="og:image:height" content="343" />
         </Head>
         <body>
           <Main />
