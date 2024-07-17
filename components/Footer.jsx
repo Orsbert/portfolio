051db5f334856edbcd062061157c3c161fd05af6
Orsbert Ayesigye <45017033+Orsbert@users.noreply.github.com>
2024-07-17 11:11:43 +0300

    Added Ug dev summit links to the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,23 +1,35 @@
-import React, { useRef } from 'react'
-import { MailButtonPlaceholder } from './MailButton'
-import { GithubLink, LinkedinLink, TwitterLink } from './Nav'
+import React, { useRef } from "react";
+import { MailButtonPlaceholder } from "./MailButton";
+import { GithubLink, LinkedinLink, TwitterLink } from "./Nav";
+import Link from "next/link";
 
 export const Footer = () => {
-
-	return (
-		<footer>
-			<div className="heading">
-				Wanna get in touch or talk about a project?
-			</div>
-			<div className="normal">
-				let’s talk
-			</div>
-			<MailButtonPlaceholder tag={'footer'} />
-			<div className="connect-icons center">
-				<TwitterLink/>
-				<LinkedinLink/>
-				<GithubLink/>
-			</div>
-		</footer>
-	)
-}
+  return (
+    <footer className='flex flex-col justify-between'>
+      <div>
+        <div className='heading'>
+          Wanna get in touch or talk about a project?
+        </div>
+        <div className='normal'>let’s talk</div>
+        <div className='inline-block'>
+          <MailButtonPlaceholder tag={"footer"} />
+        </div>
+      </div>
+      <div className='flex flex-col md:flex-row gap-2 w-full justify-between items-center p-2 pb-4  md:px-10'>
+        <div>
+          <Link
+            href='/slides/ug-dev-summit'
+            className='text-green-700 hover:text-white hover:underline transition-all'
+          >
+            My Exhibition Slides at Ug Dev Summit
+          </Link>
+        </div>
+        <div className='flex items-centergap-2'>
+          <TwitterLink />
+          <LinkedinLink />
+          <GithubLink />
+        </div>
+      </div>
+    </footer>
+  );
+};
